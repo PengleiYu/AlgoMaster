c715d3986c3c0b4726b366e6c5d1076127d0caed
yupenglei <yupenglei@kuaishou.com>
2025-04-08 19:38:44 +0800

    调整单元测试

--- test/com/example/string_search_tree/TrieSTTest.java
+++ test/com/example/string_search_tree/TrieSTTest.java
@@ -1,24 +1,40 @@
 package com.example.string_search_tree;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TrieSTTest {
 
-    @Test
-    void put() {
-        TrieST<Integer> trieST = new TrieST<>();
+    private TrieST<Integer> trieST;
+
+    @BeforeEach
+    public void init() {
+        trieST = new TrieST<>();
         trieST.put("by", 4);
         trieST.put("sea", 2);
         trieST.put("sells", 1);
         trieST.put("she", 0);
         trieST.put("shells", 3);
         trieST.put("the", 5);
+    }
+
+    @AfterEach
+    public void clear() {
+        trieST = null;
+    }
+
+    @Test
+    void printTrie() {
         TreePrinter.printTree(trieST.root);
     }
 
     @Test
     void get() {
+        assertEquals(4, trieST.get("by"));
+        assertEquals(2, trieST.get("sea"));
+        assertNull(trieST.get("hello"));
     }
 }
